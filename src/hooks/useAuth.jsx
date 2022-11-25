970e4373e423b9567289d77718206f7de523e716
unknown <maykenm7@gmail.com>
2022-11-25 17:15:00 -0500

    Actualización versión del frontend del e-commerce

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,9 @@
+import { useContext } from 'react'; // Para extraer los datos
+
+import AuthContext from '../context/AuthProvider';
+
+const useAuth = () => {
+   return useContext(AuthContext);
+};
+
+export default useAuth
